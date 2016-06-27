f2cc0349b606ed24266a49e4f5f530d5c7c8feef
Joël Kuijten <jkuijten@xebia.com>
2016-06-27 08:56:24 +0200

    added the styleSplitter regexp

--- app/scripts.babel/regExp.js
+++ app/scripts.babel/regExp.js
@@ -3,6 +3,7 @@ import {combinatorsMap} from './settings';
 export const upperCase           = /([A-Z]){1}/g;
 export const selectorSplitter    = /\s*,\s*/g;
 export const stylesSplitter      = /;\s*/g;
+export const styleSplitter       = /:\s*/g
 export const semicolonInBrackets = /\((.*?);(.*?\))/g;
 export const stylesText          = /{(.*)}/;
 export const selectorPart        = /\s+|\s?(>)\s?/;
