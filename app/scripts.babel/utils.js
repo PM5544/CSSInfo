599ee6a648c3e7164f71ad013fad5de2785f989a
Joël Kuijten <jkuijten@xebia.com>
2016-06-27 08:57:10 +0200

    added the getDomain function

--- app/scripts.babel/utils.js
+++ app/scripts.babel/utils.js
@@ -5,12 +5,18 @@ const replacers = [
   {from: /@@@/g, to: ';\n}'}
 ];
 
+export function getDomain (location) {
+  const match = /^(.*?\w)\/{1}\w/.exec(location);
+  if (match && match[1]) {
+    return match[1];
+  }
+}
 export const arraySlice = Array.prototype.slice;
 
 export function format (str) {
   replacers.forEach((replacer)=>{
     str = str.replace(replacer.from, replacer.to);
-  })
+  });
   return str;
 }
 
